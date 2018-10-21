8504fdec9b22a63e485d77919a25e171eee28248
nichub <nicolas.jeanmonod@gmail.com>
2018-10-21 23:45:15 +0200

    websocket persistant

--- Studer_A810/www/app.js
+++ Studer_A810/www/app.js
@@ -7,40 +7,68 @@ else
   var wsurl = 'ws://localhost:' + port;
   // var wsurl = 'ws://192.168.1.122:' + port;
 console.log("wsurl = " + wsurl);
-var websocket = new WebSocket(wsurl);
 
+var ws = null;
 var minus = document.getElementById('minus')
 var plus  = document.getElementById('plus')
 var value = document.getElementById('value')
 var users = document.getElementById('users')
 
 minus.onclick = function(event) {
-  websocket.send(JSON.stringify({action: 'minus'}));
+  ws.send(JSON.stringify({action: 'minus'}));
 }
 
 plus.onclick = function(event) {
-  websocket.send(JSON.stringify({action: 'plus'}));
+  ws.send(JSON.stringify({action: 'plus'}));
 }
 
-websocket.onmessage = function(event) {
-  var data = JSON.parse(event.data);
-  switch(data.type) {
-    case 'state':
-      if(data.value < 0)
-        value.classList.add("negative");
-      else
-        value.classList.remove("negative");
-      value.textContent = data.value;
-      break;
 
-    case 'users':
-      users.textContent = (
-        data.count.toString() + " user" +
-        (data.count == 1 ? "" : "s"));
-      break;
+function start() {
+  ws = null;
+  ws = new WebSocket(wsurl);
 
-    default:
-      console.error(
-        "unsupported event", data);
-  }
-};
+  ws.onopen = function(){
+    console.log('connected!');
+  };
+
+  ws.onmessage = function(event) {
+    var data = JSON.parse(event.data);
+    switch(data.type) {
+      case 'state':
+        if(data.value < 0)
+          value.classList.add("negative");
+        else
+          value.classList.remove("negative");
+        value.textContent = data.value;
+        break;
+
+      case 'users':
+        users.textContent = (
+          data.count.toString() + " user" +
+          (data.count == 1 ? "" : "s"));
+        break;
+
+      default:
+        console.error(
+          "unsupported event", data);
+    }
+  };
+
+  ws.onclose = function(){
+    console.log('closed!');
+    //reconnect now
+    check_websocket();
+  };
+}
+
+setInterval(check_websocket, 1000);
+function check_websocket(){
+    if(!ws || ws.readyState == 3)
+    {
+      console.log("Trying to connect again");
+      try {
+        start();
+      } catch(e) {}
+    }
+}
+
